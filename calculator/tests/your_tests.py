322ab73d9f3b3174ca33ef879e4df00672966276
Donnie Raymond <raymondd@stanford.edu>
2023-05-17 21:21:39 -0700

    I'm turning in the calculator assignment after finishing it.

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,7 +4,11 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
-
+assert run("1 + 1").output == "2"
+assert run("1 - 1").output == "0"
+assert run("2 \* 2").output == "4"
+assert run("6 / 3").output == "2"
+assert run("34 plus 7").exit_status != 0
 ###
 
 print("All tests passed!")
